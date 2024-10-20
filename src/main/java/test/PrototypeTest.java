776f8da1a86fa69c2f6aa8f64608b5e4251f766c
vishalm6566 <vishalm6566@gmail.com>
2024-10-20 23:54:14 +0530

    Prototype-design-pattern

--- src/main/java/test/PrototypeTest.java
+++ src/main/java/test/PrototypeTest.java
@@ -1,4 +1,5 @@
 package test;
+import designPattern.prototype.cache.BundledShapeCache;
 import designPattern.prototype.shapes.Circle;
 import designPattern.prototype.shapes.Rectangle;
 import designPattern.prototype.shapes.Shape;
@@ -47,5 +48,39 @@ public class PrototypeTest {
                 System.out.println(i + ": Shape objects are the same (booo!)");
             }
         }
+
+
+        /*
+            Prototype registry
+            You could implement a centralized prototype registry (or factory),
+            which would contain a set of pre-defined prototype objects.
+            This way you could retrieve new objects from the factory by passing its name or other parameters.
+            The factory would search for an appropriate prototype, clone it and return you a copy.
+        */
+
+
+        BundledShapeCache cache = new BundledShapeCache();
+
+        Shape shape1 = cache.get("Big green circle");
+        Shape shape2 = cache.get("Medium blue rectangle");
+        Shape shape3 = cache.get("Medium blue rectangle");
+
+        if (shape1 != shape2 && !shape1.equals(shape2)) {
+            System.out.println("Big green circle != Medium blue rectangle (yay!)");
+        } else {
+            System.out.println("Big green circle == Medium blue rectangle (booo!)");
+        }
+
+        if (shape2 != shape3) {
+            System.out.println("Medium blue rectangles are two different objects (yay!)");
+            if (shape2.equals(shape3)) {
+                System.out.println("And they are identical (yay!)");
+            } else {
+                System.out.println("But they are not identical (booo!)");
+            }
+        } else {
+            System.out.println("Rectangle objects are the same (booo!)");
+        }
+
     }
 }
